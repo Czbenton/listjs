9108d19bd5633843dabfb2a59f86bd7682450074
Zach Benton <Zach@Zach.local>
2017-05-16 14:07:57 -0500

    basic functionality in place

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 var x = document.querySelector('h1')
 
 function change() {
-  x.textContent = "new words";
+    x.textContent = "One List";
 }
 
 change();
+
+
+
+function submitText(event) {
+console.log(event);
+    if (event.which === 13) {
+        var text = document.createTextNode(event.target.value);
+        var list = document.getElementById('list');
+        var newLi = document.createElement("li");
+        list.appendChild(newLi);
+        newLi.appendChild(text);
+        document.getElementById('input').value = "";
+    }
+}
+
+
+
+
+console.log(text);
