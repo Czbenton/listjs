bafc6186986240aa5dce8d178d51ccc58193bdf9
Zach Benton <Zach@Zach.local>
2017-05-16 15:26:22 -0500

    increased funtionality

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
-var x = document.querySelector('h1')
-
-function change() {
-    x.textContent = "One List";
-}
-
-change();
-
+(function() {
+  document.querySelector('h1').textContent = "One List";
+})();
 
 
 function submitText(event) {
-console.log(event);
     if (event.which === 13) {
         var text = document.createTextNode(event.target.value);
         var list = document.getElementById('list');
         var newLi = document.createElement("li");
         list.appendChild(newLi);
+        // newLi.className = "center";
         newLi.appendChild(text);
         document.getElementById('input').value = "";
+        (function(){
+          document.getElementById('listContainer').style.display = "flex";
+        })();
     }
 }
-
-
-
-
-console.log(text);
